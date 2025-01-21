a473c4430e8f436622b6deb9f837ce746e6d6b6c
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-01-21 09:35:36 +0530

    Add files via upload

---
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/configuration/JwtokenAuthFilter.java
@@ -0,0 +1,18 @@
+package com.spring_login_web_page.Springmvc_login_system.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+
+public class JwtokenAuthFilter {
+    @Autowired
+    public JwtokenAuthFilter;
+    public String generateTokeninGamedevApp(Map<String,Object>extractClaims,UserDetails userDetails){
+        return buildToken(extractClaims,userDetails,jwtExpiration);
+    }
+
+    
+}
