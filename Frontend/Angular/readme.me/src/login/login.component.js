2f6ef80184d28bfe42ef2e74fc0831233a31ff75
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-07 10:58:38 +0530

    Add files via upload

--- Frontend/Angular/readme.me/src/login/login.component.js
+++ Frontend/Angular/readme.me/src/login/login.component.js
@@ -5,3 +5,17 @@ console.log("executing Login  Page");
 if(!LoginTest.isAuthenticated()){
   $state.TransitionTo("Login");
 }
+$scope.formSubmit=function(){
+  if(LoginService.login){
+    ($scope.username,$scope.password);
+    $rootScope.username=username;
+  }
+    $scope.error='';
+    $scope.username='';
+    $scope.password='';
+    $state.TransitionTo('mainPage');
+
+  else{
+    $scope.error('Incorrect Credentialis');
+  }
+}
