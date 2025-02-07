44551f33709600d34c9bbca0cf85701468c63a17
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-07 13:04:33 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/service/UserServices.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/service/UserServices.java
@@ -1,6 +1,8 @@
 package com.spring_login_web_page.springmvc_login_system.service;
 
+import com.spring_login_web_page.Springmvc_login_system.controller.UserController;
 import com.spring_login_web_page.springmvc_login_system.model.ApplicationUser;
+import com.spring_login_web_page.springmvc_login_system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,14 +21,30 @@ import static org.apache.el.lang.ELArithmetic.add;
 
 @Service
 public class UserServices implements UserDetailsService {
+    @Autowired
+    public User user; 
+
    @Autowired
     private PasswordEncoder passwordEncoder;
+
     @Autowired
     private ApplicationUser applicationUser;
 
+    @Autowired
+    private  UserRepository userRepository;
 
-//    @Override
-@Autowired
+    userRepository.findByUsername();
+    if(user.isPresent()){
+        user=user.get();
+        return user.builder()
+                .username(UserController.getUsername())
+                .username(UserController.getPassword())
+                .build();
+                
+    }
+
+
+    @Autowired
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         throw new UsernameNotFoundException(username+ "LoadUserBYUserName");
         System.out.println(UserDetailsService.getName());
@@ -35,9 +53,9 @@ public class UserServices implements UserDetailsService {
         return new ApplicationUser(1,"SARTHAK", "PASSWORD",roles);
     }
     Set<Role> roles=new HashSet<>();
-
+}
 //    @Override
 //   UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 //        return null;
 //    }
-}
+
