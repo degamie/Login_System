7e9b9303a28ae19ecc3819602d4e179239b31478
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-12 18:58:29 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/service/AuthenticationalService.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/service/AuthenticationalService.java
@@ -1,32 +1,44 @@
-// package com.spring_login_web_page.Springmvc_login_system.service;
+package com.spring_login_web_page.Springmvc_login_system.service;
 
-// import com.spring_login_web_page.Springmvc_login_system.model.ApplicationUser;
-// import com.spring_login_web_page.Springmvc_login_system.repository.RoleRepository;
-// import com.spring_login_web_page.Springmvc_login_system.repository.UserRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.stereotype.Service;
+import com.spring_login_web_page.Springmvc_login_system.model.ApplicationUser;
+import com.spring_login_web_page.Springmvc_login_system.repository.RoleRepository;
+import com.spring_login_web_page.Springmvc_login_system.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-// import javax.management.relation.Role;
-// import java.util.HashSet;
-// import java.util.Set;
+import javax.management.relation.Role;
+import java.util.HashSet;
+import java.util.Set;
 
-// import static jdk.internal.icu.impl.Punycode.encode;
+import static jdk.internal.icu.impl.Punycode.encode;
 
-// @Service
-// public class AuthenticationalService {
-// //    @Autowired
-//     private UserRepository userRepository;
-// //    @Autowired
-//     private RoleRepository roleRepository;
-// //    @Autowired
-//     ApplicationUser register(String username, String password) {
-//         Object encodedpassword = PasswordEncoder.encode(password);
-//         return null;
+@Service
+public class AuthenticationalService {
+   @Autowired
+    private UserRepository userRepository;
+   @Autowired
+    private RoleRepository roleRepository;
+    
+    public String getAllUserByid(String username){
+        return userRepository.save(username);
+    }
+    public String getAllUserByName(String username){
+        return userRepository.save(username);
+    }    
+    public String getAllUserByPassword(String userPassword){
+        return userRepository.save(userPassword);
+    }
 
-//     Role userRole=roleRepository.findByAuthority("USER").get();
-//     Set<Role> authorities=new HashSet<>();
-//     authorities.add(userRole);
-//     return userRepository.save(new ApplicationUser(0,username,encode(authorities)));
-//     }
-// }
+
+//    @Autowired
+    ApplicationUser register(String username, String password) {
+        Object encodedpassword = PasswordEncoder.encode(password);
+        return null;
+
+    Role userRole=roleRepository.findByAuthority("USER").get();
+    Set<Role> authorities=new HashSet<>();
+    authorities.add(userRole);
+    return userRepository.save(new ApplicationUser(0,username,encode(authorities)));
+    }
+}
