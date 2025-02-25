f07ecd229db355b5a64fdd10ca151414d2db0c56
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-25 11:45:08 +0530

    Add files via upload

---
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/configuration/JwtUtill.java
@@ -0,0 +1,13 @@
+package com.spring_login_web_page.Springmvc_login_system.configuration;
+
+public class JwtUtill {
+    public JwtUtill jwtUtill;
+    public Claims extractClaims(String token){
+        return Jwts.parser()
+                .vertifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayLoad();
+    }
+    
+}
