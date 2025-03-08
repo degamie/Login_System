66ace60feed5374a3aa0b0222e1fe237a546b58e
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-08 13:26:33 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/configuration/JwtokenAuthFilter.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/configuration/JwtokenAuthFilter.java
@@ -1,19 +1,38 @@
-// package com.spring_login_web_page.Springmvc_login_system.configuration;
+package com.spring_login_web_page.Springmvc_login_system.configuration;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import java.util.Map;
 
-// @Configuration
-// @EnableWebMvc
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ssl.SslBundleProperties.Key;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.codec.Decoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.webauthn.api.Bytes;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-// public class JwtokenAuthFilter {
-//     @Autowired
-//     public JwtokenAuthFilter;
-//     public String generateTokeninGamedevApp(Map<String,Object>extractClaims,UserDetails userDetails){
-//         return buildToken(extractClaims,userDetails,jwtExpiration);
-//     }
+@Configuration
+@EnableWebMvc
+
+public class JwtokenAuthFilter {
+    @Autowired 
+    public Key secretKey;
+    public Key Keys;
+    @Autowired
+    public byte[] KeyBytes;
+  
+    @Autowired
+    public JwtokenAuthFilter jwtokenAuthFilter;
+    public Key getSignInKey(){
+        KeyBytes=Decoder.BASE_64.decode(secretKey);
+        return Keys.hmacShaKeyFor(KeyBytes);
+    }
+    private String buildToken(Map<String extractClaims, UserDetails userDetails,JwtokenAuthFilter jwtokenAuthFilter2) { 
+            throw new UnsupportedOperationException("Unimplemented method 'buildToken'");
+}
+    public String generateTokeninGamedevApp(Map<String,Object>extractClaims,UserDetails userDetails){
+        return buildToken(extractClaims,userDetails,jwtokenAuthFilter);
+    }
+
 
     
-// }
+}
