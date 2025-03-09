840705a5c06743b9b72e475f376ec60449a0ccaf
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-09 08:52:19 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/AdminController.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/AdminController.java
@@ -9,6 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin/")
 @CrossOrigin("*")
 public class AdminController {
+    @AutoWired
+    public AdminServices adminServices;
+
+    @GetMapping("/admin/{adminName}")
+    public String getAdminName(String adminName){
+        return adminServices.getAllAdminName(adminName);
+    }
     @GetMapping("/admin/")
     public String admin() {
         return "Admin Page Logged in";
