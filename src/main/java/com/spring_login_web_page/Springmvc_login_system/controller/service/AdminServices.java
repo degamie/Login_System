8607a25a22a0754c0984612d6ed50fb37747c271
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-12 21:07:25 +0530

    Add files via upload

---
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/service/AdminServices.java
@@ -0,0 +1,20 @@
+package com.spring_login_web_page.Springmvc_login_system.service;
+
+import org.springframework.stereotype.Service;
+
+import com.spring_login_web_page.Springmvc_login_system.repository.AdminReposiotry;
+
+@Service
+public class AdminServices {
+    @AutoWired
+    public AdminReposiotry adminrepository;
+    public String getAllIdBYAdmin(String AdminId){
+        return adminrepository.getAllIdBYAdmin(AdminId);
+    }
+    public String getAllNamesBYAdmin(String adminName){
+        return adminrepository.getAllNamesBYAdmin(adminName);
+    }
+    public String getAdminPassword(String adminPassword){
+        return adminrepository.getAdminPassword(adminPassword);
+    }
+}
