356235aa2a8c3eb007c1c9c8ca32ae086b3fa0f8
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-16 20:12:20 +0530

    Update AdminServices.java

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/service/AdminServices.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/service/AdminServices.java
@@ -4,6 +4,9 @@ package com.spring_login_web_page.Springmvc_login_system.service;
 public class AdminServices {
     @AutoWired
     public AdminRepository adminrepository;
+    public String getAllAdminPhoneNum(String adminPhoneNum){//Admin Phone Num Get method Declare
+        return adminrepository.getAllAdminPhoneNum(adminPhoneNum);//Printing Admins' Phone Num
+    }
     public String getAllNamesBYAdmin(String adminName){
         return adminrepository.getAllNamesBYAdmin(adminName);
     }
