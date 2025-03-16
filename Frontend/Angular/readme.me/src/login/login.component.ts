240b072f8c02de16104ccb41d96ee0e6074d2b1c
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-16 15:57:02 +0530

    Add files via upload

--- Frontend/Angular/readme.me/src/login/login.component.ts
+++ Frontend/Angular/readme.me/src/login/login.component.ts
@@ -2,14 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-@Component({
-  selector: 'app-login',
-  imports: [FormsModule,CommonModule,NgModule]
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
-})
-export class LoginComponent {
-    username!: string;
-    Password!: string;
+// @Component({
+//   selector: 'app-login';
+//   imports: [FormsModule,CommonModule],
+//   // templateUrl: './login.component.html',
+//   styleUrl: './login.component.css';
+// })
 
+export class LoginComponent {
+  login();
 }
+    // username!: string;
+    // Password!: string;
+
+
