bf3e3248b4e4c20adadf635d4e407d33b7fe2307
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-29 12:24:43 +0530

    Update EmpServiceImpl.java

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/service/EmpServiceImpl.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/service/EmpServiceImpl.java
@@ -1,5 +1,11 @@
-// package com.spring_login_web_page.Springmvc_login_system.service;
+package com.spring_login_web_page.Springmvc_login_system.service;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
-// public class EmpServiceImpl {
-    
-// }
+public class EmpServiceImpl {//EmpServiceImpl Class Declare
+    @Autowired
+    public EmpServiceImpl empServiceImpl;//Obj declare
+        public List<Employee>getAllEmpById(String EmpId){//EmplOyeeId Service impl
+        return empService.getAllEmpId(EmpId);
+    }
+}
