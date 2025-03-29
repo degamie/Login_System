01ec0e0bd857e4747acbc4e32b353881e24e1609
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-29 12:23:59 +0530

    Create EmpRepostiory.java

---
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/EmpRepostiory.java
@@ -0,0 +1,6 @@
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpRepository extends JpaRepository<Employee,String>{//EmplOyee repository Declare
+    public List<Employee>getAllEmpById(String EmpId);//EmplOyee Id Method declare
+}
