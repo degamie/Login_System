769ca9cedb9a4e5fdd6246a1ed5d5f85a3ae3b7c
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-30 14:57:31 +0530

    Add files via upload

--- Frontend/Angular/readme.me/src/login/login.component.js
+++ Frontend/Angular/readme.me/src/login/login.component.js
@@ -2,6 +2,12 @@ var app=angular.module('MyLoginPAge','ui','router');
 app.run(function($rootScope,$location,$state,LoginTest));
 console.clear();
 console.log("executing Login  Page");
+public void login(username:String,password:String){
+  this.authenticationClient.login(username,password).subscribe((token))=>{
+    localStorage.setItem(this.tokenKey,token);
+    this.router.navigate(['/']);
+  }
+}
 if(!LoginTest.isAuthenticated()){
   $state.TransitionTo("Login");
 }
