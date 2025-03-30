9bcbf34de0a0919802e65ad53f2122be7cada348
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-30 16:35:38 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/service/EmpServiceImpl.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/service/EmpServiceImpl.java
@@ -1,11 +1,17 @@
 package com.spring_login_web_page.Springmvc_login_system.service;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 
-public class EmpServiceImpl {//EmpServiceImpl Class Declare
-    @Autowired
-    public EmpServiceImpl empServiceImpl;//Obj declare
-        public List<Employee>getAllEmpById(String EmpId){//EmplOyeeId Service impl
-        return empService.getAllEmpId(EmpId);
+import com.spring_login_web_page.Springmvc_login_system.model.Employee;
+import com.spring_login_web_page.Springmvc_login_system.repository.EmpRepository;
+
+public class EmpServiceImpl {
+    @AutoWired 
+    public EmpRepository EmpRepository;
+
+    public Employee getAllEmpName(String EmpName){
+        return EmpRepository.save(EmpName);
     }
+    public Employee getAllEmpPassword(String EmpPassword){
+        return EmpRepository.save(EmpPassword);
+    }
+    
 }
