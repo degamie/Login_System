3df519bf29fd7dc2f004c03a8e851e41c53564ba
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-30 16:36:00 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/EmployeeController.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/EmployeeController.java
@@ -1,16 +1,44 @@
-// import java.util.concurrent.ExecutorCompletionService;
+package com.spring_login_web_page.Springmvc_login_system.controller;
+import java.util.List;
+import java.util.concurrent.ExecutorCompletionService;
 
-// import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
-// @Controller
-// @RequestMapping("api/v1/employee")
-// public class EmployeeController{
-//     @AutoWired 
-//     public EmpService empService;
-//     @PostMapping(path="save")
-//     public String saveEmp(@RequestBody EmpDto empDto) {
-//         String empId=empService.addEmp(empDto);
-//         return empId;
-//     }
-// }
+import com.spring_login_web_page.Springmvc_login_system.model.Employee;
+import com.spring_login_web_page.Springmvc_login_system.service.EmpServiceImpl;
+
+
+@Controller
+@RequestMapping("api/v1/employee")
+public class EmployeeController{
+    @AutoWired 
+    public EmpServiceImpl empService;
+    @GetMapping("/Emp/{role}")
+    public List<Employee>getAllEmpByRole(String role){
+            return empService.getAllEmpByRole(role);
+    }
+    @GetMapping("/Emp/{Id}")
+    public List<Employee>getAllEmpById(String EmpId){//Empl Id getMoethod
+        return empService.getAllEmpId(EmpId);//Printing Empl Id's Fetch
+    }
+    @GetMapping("/Emp/{username}")
+    public Employee getAllUserByName(String Empname) {
+        return empService.getAllEmpName(Empname);
+    }
+    @GetMapping("/Emp/{password}")
+    public Employee getAllUserByPassword(String Password) {
+        return empService.getAllUserByPassword(Password);
+    }
+
+    
+    @PostMapping(path="save")
+    public String saveEmp(@RequestBody EmpDto empDto) {
+        String empId=empService.addEmp(empDto);
+        return empId;
+    }
+}
