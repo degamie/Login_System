60797458e75963808310664b9bfc9c0a926cda3d
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-06 21:01:21 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/EmpRepository.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/EmpRepository.java
@@ -1,12 +1,9 @@
-package com.spring_login_web_page.Springmvc_login_system.repository;
 
 import java.util.List;
 
-import com.spring_login_web_page.Springmvc_login_system.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public class EmpRepository extends JpaRepository<Employee,String>{
-    public List<Employee>getAllEmpByRole(String role);
-    public List<Employee>getEmpName(String EmpName); 
-    public List<Employee>getEmpPassword(String Password); 
-} 
+public interface EmpRepository extends JpaRepository<Employee,String>{//EmplOyee repository Declare
+    public List<Employee>getAllEmpById(String EmpId);//EmplOyee Id Method declare
+    public List<Employee>getAllEmpByName(String EmpName);//EmpName Method declare
+}
