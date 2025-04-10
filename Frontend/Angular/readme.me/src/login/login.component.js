709c313726055cc53f394b63b1363dfb3d423eb7
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-10 09:16:33 +0530

    Add files via upload

--- Frontend/Angular/readme.me/src/login/login.component.js
+++ Frontend/Angular/readme.me/src/login/login.component.js
@@ -12,6 +12,41 @@ function authInterceptor(API,auth){
 
     }
 }
+function AuthService(){
+  AuthService(HttpClient HttpClient){
+    Boolean isLoggedIn=false;
+    login(UserDetails:{userName:String,Password:String}){
+      Observable<Boolean>username;
+      return this.HttpClient.post<any>('/login',UserDetails)
+      .pipe(map(response=>{
+        localStorage.setItem('JWT_TOKEN',response.token)
+        this.isLoggedIn=true;
+        return true;
+      }));
+      catch(error){
+        console.log(error);
+        return of (false);
+      }
+        logout():void{
+          localStorage.removeItem('JWT_token',);
+          this.isLoggedIn=false;
+          isAuthenticated:Boolean;
+          return this.isLoggedIn();
+
+        }
+    
+    }
+  }
+}
+const jwt=require('jsonwebtoken');
+const verifyToken(req,res,next);
+const token=req.header('Authorization');
+if(!token)return res.status(401).json({error:'Denied Access'});
+try{
+   const decoded=jwt.verify(token,'secret-key');
+   req.userId=decoded.userId;
+   next();
+}
 public void login(username:String,password:String){
   this.authenticationClient.login(username,password).subscribe((token))=>{
     localStorage.setItem(this.tokenKey,token);
