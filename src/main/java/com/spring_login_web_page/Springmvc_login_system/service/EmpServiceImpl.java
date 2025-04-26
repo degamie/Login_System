b7269d088d4f7693a92813671e220d856cb49cf9
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-26 09:51:28 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/service/EmpServiceImpl.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/service/EmpServiceImpl.java
@@ -1,17 +1,22 @@
 package com.spring_login_web_page.Springmvc_login_system.service;
 
+import java.util.List;
+
 import com.spring_login_web_page.Springmvc_login_system.model.Employee;
 import com.spring_login_web_page.Springmvc_login_system.repository.EmpRepository;
 
 public class EmpServiceImpl {
     @AutoWired 
     public EmpRepository EmpRepository;
+    public List<Employee>getAllEmpByPhoneNum(String PhoneNum){
+        return EmpRepository.save(PhoneNum);
+    }
 
     public Employee getAllEmpName(String EmpName){
         return EmpRepository.save(EmpName);
     }
     public Employee getAllEmpPassword(String EmpPassword){
         return EmpRepository.save(EmpPassword);
-    }
+    }    
     
 }
