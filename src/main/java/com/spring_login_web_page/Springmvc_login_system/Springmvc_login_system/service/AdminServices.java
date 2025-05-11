e5807f51dd30af18ca2b41dfd632524605c3e3e4
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-11 20:22:09 +0530

    Add files via upload

---
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/Springmvc_login_system/service/AdminServices.java
@@ -0,0 +1,35 @@
+package com.spring_login_web_page.Springmvc_login_system.service;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
+import org.springframework.stereotype.Service;
+
+import com.spring_login_web_page.Springmvc_login_system.repository.AdminReposiotry;
+
+@Service
+public class AdminServices {
+    @AutoWired
+    public AdminReposiotry adminrepository;
+        public List<Admin>getAllAdminById(String AdminID){
+            return adminrepository.getAllAdminById(AdminID);
+        }
+    public List<user>findAllAdminById(String AdminID){
+        return adminrepository.findAllAdminByID(AdminID);
+    }
+        public List<Admin>setAllNamesBYAdmin (String adminName){
+            return adminrepository.findAllNamesBYAdmin(String adminName);
+        }
+    public String getAllAdminBYPhoneNum(String PhoneNum){
+        return adminrepository.getAllAdminBYPhoneNum(PhoneNum);
+    }
+    public String getAllIdBYAdmin(String AdminId){
+        return adminrepository.getAllIdBYAdmin(AdminId);
+    }
+    public String getAllNamesBYAdmin(String adminName){
+        return adminrepository.getAllNamesBYAdmin(adminName);
+    }
+    public String getAdminPassword(String adminPassword){
+        return adminrepository.getAdminPassword(adminPassword);
+    }
+}
