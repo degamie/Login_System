19bfea85a549144b47c3b81926d69c3f60f835c1
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-11 20:22:38 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/Springmvc_login_system/controller/AdminController.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/Springmvc_login_system/controller/AdminController.java
@@ -1,14 +1,54 @@
 package com.spring_login_web_page.Springmvc_login_system.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring_login_web_page.Springmvc_login_system.service.AdminServices;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
 @RequestMapping("/admin/")
 @CrossOrigin("*")
 public class AdminController {
+    @AutoWired
+    public AdminServices adminServices;
+    @GetMapping("/admin/set/{AdminID}")
+    public List<Admin>getAllAdminById(String AdminID){return adminrepository.getAllAdminById(AdminID);}//fetching AdminId
+      
+    // @GetMapping("/admin/set/{AdminID}")
+    // public List<user>findAllAdminById(String AdminID){
+    //     return adminServices.findAllAdminById(AdminID);
+    // }
+       @PostMapping("/admin/set/{adminName}")
+         public List<Admin>setAllNamesBYAdmin (String adminName){
+            return adminServices.setAllAdminByPhoneNum(adminName);
+         }
+      
+    @GetMapping("/admin/{adminPhoneNum}")
+    public String getAllAdminByPhoneNum(String PhoneNum) {
+        return adminServices.getAllAdminByPhoneNum(PhoneNum);
+    }
+    
+    @GetMapping("/admin/{adminId}")
+    public String getAllIdBYAdmin(String adminId) {
+        return adminServices.getAllIdBYAdmin(adminId);
+    }
+    
+
+    @GetMapping("/admin/{adminName}")
+    public String getAdminName(String adminName){
+        return adminServices.getAllAdminName(adminName);
+    }
+    @GetMapping("/admin/{adminPassword}")
+    public String getAdminPassword(String adminPassword){
+        return adminServices.getAllAdminPassword(adminPassword);
+    }
     @GetMapping("/admin/")
     public String admin() {
         return "Admin Page Logged in";
