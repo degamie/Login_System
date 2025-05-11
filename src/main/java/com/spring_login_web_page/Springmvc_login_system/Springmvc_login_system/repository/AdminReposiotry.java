0cc6a4b2b05825cf5a3e18e378c92d8ed0a3b859
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-11 20:19:51 +0530

    Add files via upload

---
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/Springmvc_login_system/repository/AdminReposiotry.java
@@ -0,0 +1,19 @@
+package com.spring_login_web_page.Springmvc_login_system.repository;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AdminReposiotry extends JpaRepository<Admin,String>{
+    public List<Admin>getAllAdminById(String AdminID);
+    public List<user>findAllAdminById(String AdminID);
+    public List<Admin> findAllNamesBYAdmin (String adminName); 
+    public List<Admin>setAllNamesBYAdmin (String adminName);
+    public List<Admin> getAllAdminBYPhoneNum (String PhoneNum);
+    public List<Admin> getAllIdBYAdmin (String adminId);
+    public List<Admin> getAllNamesBYAdmin (String adminName);
+    public List<Admin> getAdminAllPassword (String adminPassword);
+}
