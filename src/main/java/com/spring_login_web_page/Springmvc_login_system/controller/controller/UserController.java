673bc8d196ee337721ef8138c6140bf36a05e55e
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-24 20:00:01 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/controller/UserController.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/controller/UserController.java
@@ -2,10 +2,15 @@ package com.spring_login_web_page.Springmvc_login_system.controller;
 
 import com.spring_login_web_page.springmvc_login_system.service.UserServices;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,36 +20,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 @CrossOrigin("*")
-
+@Controller
 
 public class UserController {
-    //Printing Methods
-    @GetMapping("/user/req/login")
-    public String AccessUserPage() {return "Login Page Is Successfully Accessed By User";}
-    @GetMapping("/user/req/register")
-    public String RegisterAccessUserPage() {return "Register Page Is Successfully Accessed By User";}
-    @GetMapping("/user/req/signup")
-    public String SignupAccessUserPage() {return "Signup Page Is Successfully Accessed By User";}
-
+    public String UserId;
     @Autowired
-    public UserServices UserService;
-    @GetMapping("/User/{Id}")
-    public User getAllUserId(String Id){
-        return UserService.getAllUserPassword(Id);
+    public UserServices userService;
+    @GetMapping("/User/{UserId}")
+    public List<userService> getAllUserId(@PathVariable String UserId,@RequestBody User user){//Fetching The UserId's End Pt. Val
+        return userService.getAllUserId(UserId);
     }
-    @GetMapping("/User/{Password}")
-    public String getAllUserPassword(@RequestParam String Password) {
-        return UserServices.getAllUserPassword(Password);
-    }
-    @GetMapping("/User/{username}")
-    public String getAllUsername(@RequestParam String username) {
-        return UserService.getAllUsername(username);
-    }
-    @GetMapping("/User/{proffession}")
-    public String getAllUserproffession(@RequestParam String proffession) {
-        return UserService.getAllUserproffession(proffession);
-    }
-
-
-
 }
+    // //Printing Methods
+    // @GetMapping("/user/req/login")
+    // public String AccessUserPage() {return "Login Page Is Successfully Accessed By User";}
+    // @GetMapping("/user/req/register")
+    // public String RegisterAccessUserPage() {return "Register Page Is Successfully Accessed By User";}
+    // @GetMapping("/user/req/signup")
+    // public String SignupAccessUserPage() {return "Signup Page Is Successfully Accessed By User";}
+
+
+    // @GetMapping("/User/{Password}")
+    // public String getAllUserPassword(@RequestParam String Password) {
+    //     return UserServices.getAllUserPassword(Password);
+    // }
+    // @GetMapping("/User/{username}")
+    // public String getAllUsername(@RequestParam String username) {
+    //     return UserService.getAllUsername(username);
+    // }
+    // @GetMapping("/User/{proffession}")
+    // public String getAllUserproffession(@RequestParam String proffession) {
+    //     return UserService.getAllUserproffession(proffession);
+    // }
+
