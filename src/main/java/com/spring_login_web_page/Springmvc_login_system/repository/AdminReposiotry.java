161652d2d53e0bb301b07b51b5a1afe487082901
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-28 10:33:08 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/AdminReposiotry.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/AdminReposiotry.java
@@ -2,19 +2,22 @@ package com.spring_login_web_page.Springmvc_login_system.repository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class AdminReposiotry extends JpaRepository<Admin,String>{
-    public void setAllAdminBYPhoneNum(String PhoneNum);
-    public List<Admin>getAllAdminById(String AdminID);
-    public List<user>findAllAdminById(String AdminID);
-    public List<Admin> findAllNamesBYAdmin (String adminName); 
+public interface AdminReposiotry extends JpaRepository<Admin,String>{
+    @Autowired
+    public Admin admin;
+    public <admin> setAllAdminBYPhoneNum(String PhoneNum);
+    public List<admin>getAllAdminById(String AdminID);
+    public List<admin>findAllAdminById(String AdminID);
+    public List<admin> findAllNamesBYAdmin (String adminName); 
     public List<Admin>setAllNamesBYAdmin (String adminName);
-    public List<Admin> getAllAdminBYPhoneNum (String PhoneNum);
-    public List<Admin> getAllIdBYAdmin (String adminId);
-    public List<Admin> getAllNamesBYAdmin (String adminName);
-    public List<Admin> getAdminAllPassword (String adminPassword);
+    public List<admin> getAllAdminBYPhoneNum (String PhoneNum);
+    public List<admin> getAllIdBYAdmin (String adminId);
+    public List<admin> getAllNamesBYAdmin (String adminName);
+    public List<admin> getAdminAllPassword (String adminPassword);
 }
