a6d230492a8b8e848d0fb5d9a93aaceb41076fd0
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-31 19:12:36 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/service/AdminServices.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/service/AdminServices.java
@@ -1,5 +1,8 @@
 package com.spring_login_web_page.Springmvc_login_system.service;
 
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.stereotype.Service;
 
 import com.spring_login_web_page.Springmvc_login_system.repository.AdminReposiotry;
@@ -8,6 +11,21 @@ import com.spring_login_web_page.Springmvc_login_system.repository.AdminReposiot
 public class AdminServices {
     @AutoWired
     public AdminReposiotry adminrepository;
+        public String getAllAdminById(String AdminID){
+            return adminrepository.saveAllAdminById(AdminID);
+        }
+    public String findAllAdminById(String AdminID){
+        return adminrepository.findAllAdminByID(AdminID);
+    }
+        public String setAllNamesBYAdmin (String adminName){
+            return adminrepository.findAllNamesBYAdmin(String adminName);
+        }
+    public String setAllAdminBYPhoneNum(String PhoneNum){
+        return adminrepository.findAllAdminBYPhoneNum(PhoneNum);
+    }
+    public String getAllAdminBYPhoneNum(String PhoneNum){
+        return adminrepository.getAllAdminBYPhoneNum(PhoneNum);
+    }
     public String getAllIdBYAdmin(String AdminId){
         return adminrepository.getAllIdBYAdmin(AdminId);
     }
