24a8d7c5ce919bb8c8ab671345dbbdb8c414531d
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-18 20:00:23 +0530

    Add files via upload

---
+++ login-sys/src/login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function login() {
+    const onSubmit = (data) => {
+        const userData=localStorage.setItem("data.email", JSON.stringify({name: data.name, password: data.password}));
+        if(userData){
+            if(userData.password===data.password){
+                console.log(userData.name+"Logged In Successfully!");}
+            }
+
+  return (
+    <div>login</div>
+  )
+}
+}
+
+export default login;
+//this.Ref = React.createRef()
