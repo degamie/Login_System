caab839d6b001fab68db08f6125ecf79f9db368e
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-18 17:01:56 +0530

    Add files via upload

---
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/service/RoleServices.java
@@ -0,0 +1,12 @@
+package com.spring_login_web_page.Springmvc_login_system.service;
+
+import com.spring_login_web_page.Springmvc_login_system.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+ @Service
+    public class RoleServices {
+        @Autowired
+        public RoleRepository roleRepository;
+        public String getAllByUserId(String UserId){return roleRepository.saveAllByUserId(UserId); }
+}
