dbd81eaa93e67f7bccca3c1ac9e9ec6f7a8b6f3a
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-19 12:16:51 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/RoleController.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/RoleController.java
@@ -3,10 +3,14 @@ package com.spring_login_web_page.Springmvc_login_system.controller;
 import com.spring_login_web_page.Springmvc_login_system.service.RoleServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RoleController {
     @Autowired
     public RoleServices roleServices;
     public String getAllByUserId(String UserId){return roleServices.getAllByUserId(UserId); }
+    @GetMapping("/saveAll/{UserPassword}")
+    public String getAllByUserPassword (String UserPassword){return roleServices.getAllByUserPassword(UserPassword);}
+
 }
