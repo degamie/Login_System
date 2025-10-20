0bf3925db9077622b029da0a768b2ccea9802d3d
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-20 14:04:38 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/UserRepository.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/UserRepository.java
@@ -1,8 +1,8 @@
-package com.spring_login_web_page.Springmvc_login_system.repository;
+package com.spring_login_web_page.springmvc_login_system.repository;
 
-import com.spring_login_web_page.Springmvc_login_system.model.ApplicationUser;
+import com.spring_login_web_page.springmvc_login_system.model.ApplicationUser;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,21 +10,10 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<ApplicationUser, String> {
-    public List<String> registeruser(String username, String password);
-    Optional<ApplicationUser> findUserByUsername(String username);
-    Optional<ApplicationUser>findUserBypassword(String password);
-    Optional<ApplicationUser>findUserById(String id);
-    public Optional<User> getUserid(String userid);
-    public Optional<User> setUserid(String userid);
-    public List<String> getUserById(String userid);
-    public List<String> getUserByusername(String username);
-    public List<User> getAllUsers();
-    public List<ApplicationUser> getAllUsersByRole(String role);
-
-
-
+    public List<ApplicationUser> getUserId(Integer UserId);
+    public List<ApplicationUser>setUserId(Integer UserId);
+    static Optional<ApplicationUser> findByUsername(String username);
+    public String saveAllBYAuthorities(List<SecurityProperties.User>Authorities);
+    public List<ApplicationUser>getUsername(String UserName);
 
 }
-//    public List<ApplicationUser> findByUsername(String username);
-//    public List<ApplicationUser> loadUserByUsername(String username);
-
