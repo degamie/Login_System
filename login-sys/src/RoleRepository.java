086bfd03c0ed7c6b50273c16c4e3d49f84b23a40
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-22 15:18:12 +0530

    Add files via upload

---
+++ login-sys/src/RoleRepository.java
@@ -0,0 +1,16 @@
+package com.spring_login_web_page.Springmvc_login_system.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.management.relation.Role;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    public String findAllByUserId(String UserId);
+    public String saveAllByUserId(String UserId);
+    public String saveAllByUserPassword(String UserPassword);
+    Optional<Role> findByName(String name);
+    Optional<Role> findByAuthority(String authority);
+}
