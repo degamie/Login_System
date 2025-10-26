45206bd15f50b02b7789b2d89b7425aa13724a1a
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-26 12:35:11 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/service/RoleServices.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/service/RoleServices.java
@@ -11,4 +11,5 @@ import org.springframework.stereotype.Service;
         public String getAllByUserId(String UserId){return roleRepository.saveAllByUserId(UserId); }
      public String getAllByUserPassword (String UserPassword){return roleRepository.saveAllByUserPassword(UserPassword);}
          public String getAllByUserName(String UserName){return roleRepository.saveAllByUserName(UserName);}
+     public String getAllByauthority(String authority){return roleRepository.saveAllByauthority(authority);}
 }
