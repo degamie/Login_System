916acb8e12901fac7555f7d5d33248618598c993
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-29 10:43:56 +0530

    Add files via upload

--- login-sys/public/login.jsx
+++ login-sys/public/login.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 function login() {
+  const getData=(data)=>{
+    return data;
+  }
+const setData=(data)=>{
+    this.data=data;}
     const onSubmit = (data) => {
         const userData=localStorage.setItem("data.email", JSON.stringify({name: data.name, password: data.password}));
         if(userData){
@@ -8,8 +13,25 @@ function login() {
                 console.log(userData.name+"Logged In Successfully!");}
             }
         else console.log("User Email and Password Unmatched");
-
+          }
+          const getPassword=(data.Password)=>{return Password;}
   return (
+      <h2>Login Form</h2>
+      <form className="App" OnSubmit={handleSubmit(OnSubmit)}>
+          <input
+            type="Email"
+            {register("email")}
+            {required:true}
+            placeholder="Email"/>
+ {errors.email && <span style= {{<color:"red"}}>*Email* is compulsory</span>}
+              <input
+                        type="Password"
+                        {register("Password")}
+                        {required:true}
+                        placeholder="Password"/>
+                          {errors.Password && <span style= {{<color:"red"}}>*Password* is compulsory</span>}
+              <input type="Submit" style={backgroundColor:"#a1eafb"}>
+
     <div>login</div>
   )
 }
