b9957e30242c0e974c04ec6e346137cb9ef02456
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-11-21 19:55:56 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/Springmvc_login_system/repository/RoleRepository.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/Springmvc_login_system/repository/RoleRepository.java
@@ -1,11 +1,22 @@
-package com.spring_login_web_page.Springmvc_login_system.repository;
+package com.spring_login_web_page.Springmvc_login_system.repository.src.main.java.com.spring_login_web_page.Springmvc_login_system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.management.relation.Role;
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    public String findAllByUserId(String UserId);
+    public String saveAllByUserId(String UserId);
+    public String saveAllByUserPassword(String UserPassword);
+   public String findAllByUserPassword(String UserPassword );
     Optional<Role> findByName(String name);
+    public String saveAllByUserName(String UserName);
     Optional<Role> findByAuthority(String authority);
+    public String updateAllByAuthority(String authority);
+    public String saveAllByauthority(String authority);
+     public String saveAllPhoneNumber(String PhoneNumber);
+     public void findAllPhoneNumber(String PhoneNumber);//Finding PhoneNumber in Server
 }
