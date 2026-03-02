ab641647fc472128700436af93b320ab29a4291e
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-02 22:14:55 +0530

    Add files via upload

--- login-sys/src/login_component.js
+++ login-sys/src/login_component.js
@@ -1,4 +1,7 @@
-//WIP(02/03/2026)(Sarthak Mittal)#1
+//WIP(02/03/2026)(Sarthak Mittal)#1.1
 function login{
+const [email,setEmail]=useState('');
+const [password,setPassword]=useState('');
+const [error,seterror]=useState('');
 var username="Sarthak";
 }
