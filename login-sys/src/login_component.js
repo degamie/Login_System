a00d067570555833bb86af2c1991a91c521d2d66
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-03 19:18:52 +0530

    Add files via upload

--- login-sys/src/login_component.js
+++ login-sys/src/login_component.js
@@ -1,7 +1,11 @@
-//WIP(02/03/2026)(Sarthak Mittal)#1.1
+//WIP(03/03/2026)(Sarthak Mittal)#1.1.1 
+import React,{useState} from 'react';
 function login{
 const [email,setEmail]=useState('');
 const [password,setPassword]=useState('');
 const [error,seterror]=useState('');
 var username="Sarthak";
+}
+const handleChane=(e)={
+setCredentials({...credentials,[e.target.name]:e.target.value});
 }
