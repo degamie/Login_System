a65fe21499bb9d6b66a7f505c2b3e055589c02de
Degamie_sign <degamiesign@gmail.com>
2026-03-04 16:47:17 +0530

    New Commitk #1.1

--- login-sys/src/login_component.js
+++ login-sys/src/login_component.js
@@ -1,7 +1,44 @@
-//WIP(02/03/2026)(Sarthak Mittal)#1.1
-function login{
+//WIP(04/03/2026)(Sarthak Mittal)#1.1.1.1.1
+import React,{useState} from 'react';
+function login(){
+    register,
 const [email,setEmail]=useState('');
 const [password,setPassword]=useState('');
 const [error,seterror]=useState('');
 var username="Sarthak";
 }
+const handleChane=(e)={
+setCredentials({...credentials,[e.target.name]:e.target.value});
+}
+const OnSubmit=(data)=>{
+const userData=JSON.parse(LocalStorage.getItem(data.email));
+if(userData){
+console.log("UserData Successfully logged in");
+else console.log("Email nd Password Not matched and Not Found!,Pls Try Again!");
+
+}
+return (
+        <>
+            <h2>Login Form</h2>
+
+            <form className="App" onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    type="email"
+                    {...register("email", { required: true })}
+                    placeholder="Email"
+                />
+                {errors.email && <span style={{ color: "red" }}>*Email* is mandatory</span>}
+
+                <input
+                    type="password"
+                    {...register("password", { required: true })}
+                    placeholder="Password"
+                />
+                {errors.password && <span style={{ color: "red" }}>*Password* is mandatory</span>}
+
+                <input type="submit" style={{ backgroundColor: "#a1eafb" }} />
+            </form>
+        </>
+    );
+}
+
