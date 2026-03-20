44b75d3039454b49aaa754f7f4cc4ec59608a0f3
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-20 10:40:58 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/UserRepository.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/UserRepository.java
@@ -1,6 +1,8 @@
+//WID(20/03/2026)(Sarthak Mittal)#1.1.1
 package com.spring_login_web_page.Springmvc_login_system.repository;
 
-import com.spring_login_web_page.springmvc_login_system.model.ApplicationUser;
+
+import com.spring_login_web_page.Springmvc_login_system.model.*;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,9 +12,14 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<ApplicationUser, String> {
+    public List<ApplicationUser>findByPassword(String Password);
+    public List<ApplicationUser> existsByAuthority(String authority);
+    public List<ApplicationUser> updateByAuhtority(String authority);
+    public List<ApplicationUser> findByAuthority(String authority);
+    public List<ApplicationUser> findByUserName(String UserName);
     public List<ApplicationUser> getUserId(Integer UserId);
     public List<ApplicationUser>setUserId(Integer UserId);
-    public static Optional<ApplicationUser> findByUsername(String username);
+    public List<ApplicationUser>saveByUsername(String username);
     public String saveAllBYAuthorities(List<SecurityProperties.User>Authorities);
     public List<ApplicationUser>getUsername(String UserName);
     public String saveAllByPassword(String Password);
