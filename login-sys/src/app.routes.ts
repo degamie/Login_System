87ec6e733352ada829bf65064463f0f0c0ef91d3
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-09 09:07:44 +0530

    Add files via upload

---
+++ login-sys/src/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { AuthGuardLoginComponent } from '../auth-guard-login/auth-guard-login.component';
+
+export const routes: Routes = [
+    imports[
+        providers[
+            AuthGuardLoginComponent;
+        ]
+    ]
+
+];
---
+++ login-sys/src/employees/Employee.ts
@@ -0,0 +1,5 @@
+//WID(9/5/2026)(Sarthak Mittal)(DegamieSign)
+interface  Employee{
+    empId:'1',
+    empName:'abc'
+}
