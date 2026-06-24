c987acaf9b562ca6ae5b83766d45c205544f2907
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-06-24 11:06:31 +0530

    Add files via upload

---
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/service/RoleServices.java
@@ -0,0 +1,27 @@
+//WID(24/6/2026)(Sarthak Mittal(RoleRepository)
+package com.spring_login_web_page.Springmvc_login_system.service;
+
+//
+//import com.spring_login_web_page.Springmvc_login_system.repository.src.main.java.com.spring_login_web_page.Springmvc_login_system.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.management.relation.Role;
+import java.util.List;
+
+@Service
+    public class RoleServices {
+        @Autowired
+        public RoleRepository roleRepository;
+        RoleServices(RoleRepository roleRepository){this.roleRepository=roleRepository;}//Binding RoleRepo in App
+        public String getAllByUserId(String UserId){return roleRepository.saveAllByUserId(UserId); }
+     public String getAllByUserPassword (String UserPassword){return roleRepository.saveAllByUserPassword(UserPassword);}
+         public String getAllByUserName(String UserName){return roleRepository.saveAllByUserName(UserName);}
+     public String getAllByauthority(String authority){return roleRepository.saveAllByauthority(authority);}
+     public String getAllPhoneNumber(String PhoneNumber){
+            return roleRepository.saveAllPhoneNumber(PhoneNumber);
+     }
+    public List<Role>  getByRoleName(String roleName){
+        return roleRepository.findByRoleName(roleName);
+    }
+}
